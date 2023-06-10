a749e333729d6b6a2b1ed43ceb36d72ef819d0bc
Darginid <130227478+darginid@users.noreply.github.com>
2023-06-10 19:40:06 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 $(function () {
+
    $('.slider').slick({
       slidesToShow: 3,
       slidesToScroll: 3,
       arrows: false,
-      dots: true
-    });
+      dots: true,
+      responsive: [
+         {
+            breakpoint: 1175,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2,
+            }
+         },
+         {
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+            }
+         }
+      ]
+   });
+
+   $('.nav__btn').on('click', function () {
+      $('.nav__list').toggleClass('nav__list--active')
+   });
+
 });
